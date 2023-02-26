d9b823888635c8b5f960f33cffffa90bb450c8bf
Code Sandbox <hello@codesandbox.io>
2023-02-26 06:39:52 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.05c927a9d6780db5286b5ab484615e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e404bb313be897c420e0b69402b82729",
+    "url": "https://garv-shah.github.io/csb-gu5ncf/index.html"
+  },
+  {
+    "revision": "ab4110f2ea1851aaac61",
+    "url": "https://garv-shah.github.io/csb-gu5ncf/static/css/2.024a18e1.chunk.css"
+  },
+  {
+    "revision": "91887ed9eefdd281bca4",
+    "url": "https://garv-shah.github.io/csb-gu5ncf/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "ab4110f2ea1851aaac61",
+    "url": "https://garv-shah.github.io/csb-gu5ncf/static/js/2.93806118.chunk.js"
+  },
+  {
+    "revision": "91887ed9eefdd281bca4",
+    "url": "https://garv-shah.github.io/csb-gu5ncf/static/js/main.90273737.chunk.js"
+  },
+  {
+    "revision": "5c12c5ce86f0461a35f8",
+    "url": "https://garv-shah.github.io/csb-gu5ncf/static/js/runtime~main.13b18651.js"
+  }
+]);
